899f21842d7e6907ff58f6ea04e1927ab4b4cc30
chia313339 <chia313339@gmail.com>
2025-06-05 23:01:44 +0800

    20250605

--- src/views/Page54.vue
+++ src/views/Page54.vue
@@ -47,6 +47,26 @@
         <transition name="fade">
           <div v-if="selectedItem">
             <img :src="getImageSrc(selectedItem)" alt="Selected Image" class="centered-image" :key="selectedItem" />
+            <div  v-if="selectedItem === '淨水器'">
+              <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img src="/img/p54/淨水器001.png" class="d-block w-100" alt="...">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="/img/p54/淨水器002.png" class="d-block w-100" alt="...">
+                  </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Next</span>
+                </button>
+                </div>
+            </div>
             <div  v-if="selectedItem === '櫃體'">
               <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
                 <div class="carousel-inner">
@@ -65,24 +85,6 @@
                   <div class="carousel-item">
                     <img src="/img/p54/櫃體005.png" class="d-block w-100" alt="...">
                   </div>
-                  <!-- <div class="carousel-item">
-                    <img src="/img/p54/櫃體006.png" class="d-block w-100" alt="...">
-                  </div>
-                  <div class="carousel-item">
-                    <img src="/img/p54/櫃體007.png" class="d-block w-100" alt="...">
-                  </div>
-                  <div class="carousel-item">
-                    <img src="/img/p54/櫃體008.png" class="d-block w-100" alt="...">
-                  </div>
-                  <div class="carousel-item">
-                    <img src="/img/p54/櫃體009.png" class="d-block w-100" alt="...">
-                  </div>
-                  <div class="carousel-item">
-                    <img src="/img/p54/櫃體010.png" class="d-block w-100" alt="...">
-                  </div>
-                  <div class="carousel-item">
-                    <img src="/img/p54/櫃體011.png" class="d-block w-100" alt="...">
-                  </div> -->
                 </div>
                 <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
                   <span class="carousel-control-prev-icon" aria-hidden="true"></span>
